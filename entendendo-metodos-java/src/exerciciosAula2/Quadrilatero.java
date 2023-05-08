d5349f554f14c7365a135363cf15d79fe4873c9c
DalineHeld <dalineheld@hotmail.com>
2023-05-08 13:33:29 -0300

    att quadrilatero

--- entendendo-metodos-java/src/exerciciosAula2/Quadrilatero.java
+++ entendendo-metodos-java/src/exerciciosAula2/Quadrilatero.java
@@ -14,7 +14,9 @@ public class Quadrilatero {
         System.out.println("Área do trapézio:" + ((baseMaior+baseMenor)*altura) / 2);
     }
 
-    public static void area(float diagonal1, float diagonal2) {
+    public static void area(float diagonal1, float diagonal2) { //aqui mudou o tipo de dado para não dar erro
+        //pq o sistema se confunde caso tenham métodos com nomes iguais. quantidades de parâmetros iguais e 
+        //tipos de dados iguais
         System.out.println("Área do losango:" + (diagonal1 * diagonal2)/2);
     }
 }
